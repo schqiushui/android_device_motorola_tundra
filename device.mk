e54f746f90c5c0364a9b55f64456bbc134b96876
schqiushui <orochi9999@gmail.com>
2024-02-18 15:17:49 +0800

    [Temp Drop]Revert "tundra: Import prebuilt Moto Dolby"
    This reverts commit e91fcd051cb7d93a7b5745f922386e7c31d2e230.

--- device.mk
+++ device.mk
@@ -80,7 +80,6 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_lahaina.xml \
     $(LOCAL_PATH)/media/media_codecs_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
     $(LOCAL_PATH)/media/media_profiles_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_lahaina.xml
